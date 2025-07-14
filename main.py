bb6e6dab8f473bb5fc32b18b8068da6340f5a563
Khoa Nguyen <nguyendangkhoa211@gmail.com>
2025-07-15 07:11:21 +0800

    create a streamlit documentation assistant chat bot

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
-def main():
-    print("Hello from documentation-assistant-project!")
+from backend.core import run_llm
+import streamlit as st
+from typing import Set
 
 
-if __name__ == "__main__":
-    main()
+st.header("LangChain - Documentation Assistant")
+
+prompt = st.text_input("Prompt", placeholder="Enter your prompt here...")
+
+
+if "user_prompt_history" not in st.session_state:
+    st.session_state["user_prompt_history"] = []
+
+if "chat_answer_history" not in st.session_state:
+    st.session_state["chat_answer_history"] = []
+
+
+def create_sources_string(source_urls: Set[str]) -> str:
+    if not source_urls:
+        return ""
+    sources_list = list(source_urls)
+    sources_list.sort()
+    sources_string = "sources:\n"
+    for i, source in enumerate(sources_list):
+        sources_string += f"{i+1}. {source}\n"
+    return sources_string
+
+
+if prompt:
+    with st.spinner("Generating response..."):
+        generated_response = run_llm(query=prompt)
+        # extract URLs
+        sources = set([doc.metadata["source"] for doc in generated_response["context"]])
+
+        formatted_response = (
+            f"{generated_response["answer"]} \n\n {create_sources_string(sources)}"
+        )
+
+        st.session_state["user_prompt_history"].append(prompt)
+        st.session_state["chat_answer_history"].append(formatted_response)
+
+if st.session_state["chat_answer_history"]:
+    for generated_response, user_query in zip(
+        st.session_state["chat_answer_history"], st.session_state["user_prompt_history"]
+    ):
+        st.chat_message("user").write(user_query)
+        st.chat_message("assistant").write(generated_response)
